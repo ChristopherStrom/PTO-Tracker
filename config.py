e49103353af6404a12816f59665efea80681ea6e
Christopher Strom <cstrom@clsio.com>
2024-07-09 21:47:34 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
-import secrets
 
 class Config:
-    SECRET_KEY = secrets.token_hex(16)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
