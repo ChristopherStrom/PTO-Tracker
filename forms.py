9166f3e7bde6b8936a37b002230b6185169ea01a
Christopher Strom <cstrom@clsio.com>
2024-07-10 09:56:10 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,4 +1,3 @@
-# forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField, FloatField, SelectField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
