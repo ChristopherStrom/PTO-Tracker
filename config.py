a5ec4a638501c2ef25b50bb1fc78bc59d8414b85
Christopher Strom <cstrom@clsio.com>
2024-07-10 17:36:04 -0500

    Update config.py
    limit session to 1 hour

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-import os
+from datetime import timedelta
 
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PERMANENT_SESSION_LIFETIME = timedelta(hours=1)  # Set session limit to 1 hour
