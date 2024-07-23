bc5ba7618e18b80c683e3e992c6931c01f247c17
Christopher Strom <cstrom@clsio.com>
2024-07-23 18:11:59 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
 
 db = SQLAlchemy()
 login_manager = LoginManager()
