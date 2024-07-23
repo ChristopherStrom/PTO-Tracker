0d37855ef76d8a76f21614d59755bbdf0d5004f4
Christopher Strom <cstrom@clsio.com>
2024-07-23 18:24:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager, UserMixin, login_user, current_user, logou
 from flask_migrate import Migrate
 from config import Config
 from models import db, login_manager, User, TimeOff, BucketChange
-from forms import LoginForm, AddUserForm, EditUserForm, TimeOffForm, AddTimeForm, EditBucketForm
+from forms import LoginForm, AddUserForm, EditUserForm, TimeOffForm, AddTimeForm, EditBucketForm, NoteForm
 from datetime import datetime, timedelta
 from flask_wtf.csrf import CSRFProtect
 from sqlalchemy import func
