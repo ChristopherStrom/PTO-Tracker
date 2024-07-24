bb388faac3aec26177ed479470b9019b527d8e93
Christopher Strom <cstrom@clsio.com>
2024-07-24 17:36:12 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site2.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(hours=1)  # Set session limit to 1 hour
