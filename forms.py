9af10f1a3fdbf81203464160c5bd7093def4757f
Christopher Strom <cstrom@clsio.com>
2024-07-24 09:32:08 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField, FloatField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from models import User
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
